f10f5a839243bc84469a350d04fbb3325df3f762
thedark6903-hue <thedark6903@gmail.com>
2026-08-13 07:57:22 +0530

    Refactor version constants to uppercase naming

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,6 @@
-export const versionCode = 11;
-export const versionName = "1.11";
-export const apkUrl =
+export const APP_VERSION = "1.11";
+
+export const VERSION_CODE = 11;
+
+export const APK_URL =
   "https://github.com/thedark6903-hue/hello-test/releases/download/v1.11/app-debug.apk";
