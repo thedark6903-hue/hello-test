74ea75a2e682e9c9b4f50679115f1eca157eb7f5
thedark6903-hue <thedark6903@gmail.com>
2026-08-14 07:21:01 +0530

    Update version.ts

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,5 @@
-export const APP_VERSION = "1.11";
-
-export const VERSION_CODE = 11;
+export const APP_VERSION = '1.14';
+export const VERSION_CODE = 14;
 
 export const APK_URL =
   "https://github.com/thedark6903-hue/hello-test/releases/download/v1.11/app-debug.apk";
